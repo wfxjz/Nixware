7c640f18100a95f59d77bc65caa1cba80ea9fc8a
svn <ipsvn@protonmail.com>
2024-04-04 01:39:03 +0100

    Bump version to 2.0.5

--- ChaseMod.cs
+++ ChaseMod.cs
@@ -17,7 +17,7 @@ public class ChaseMod : BasePlugin, IPluginConfig<ChaseModConfig>
 
     public override string ModuleName => "HnS ChaseMod";
     public override string ModuleAuthor => "svn";
-    public override string ModuleVersion => "2.0.4";
+    public override string ModuleVersion => "2.0.5";
 
     public ChaseModConfig Config { get; set; } = new();
     public void OnConfigParsed(ChaseModConfig config) { Config = config; }
